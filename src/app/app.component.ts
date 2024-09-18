0a2e24b9f3f8fba69c32223e899f5f2f234a55a7
erneto13 <erneto.amaral13@gmail.com>
2024-09-17 20:47:50 -0700

    feat: agregó la importación del módulo HttpClient para proporcionar el servicio de cliente HTTP en la aplicación aplicación.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,5 +10,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gestionVehiculos';
+  userList: any[] = [];
+  constructor(private http: HttpClient) {
+  }
+
+  getAllUsers() {
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((resultado:any) => {
+      this.userList = resultado;
+    });
+  }
 }
