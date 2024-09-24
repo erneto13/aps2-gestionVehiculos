79ac8b11913eefc59e3ab7cd95fe7a421a7c5cf8
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 03:36:27 -0700

    add: Interfaces de Contactos y Credenciales

---
+++ src/app/model.ts
@@ -0,0 +1,14 @@
+export interface Contacts {
+    id: number,
+    name: string,
+    company: string,
+    phone: string,
+    email: string,
+    service: string,
+    status: string,
+}
+
+export interface Credentials {
+    email: string,
+    password: string,
+}
