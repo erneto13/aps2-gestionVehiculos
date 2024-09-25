9b1009d7ca252c20391ccbf79355a2d39baaa140
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 23:20:09 -0700

    change: Se movieron los anteriores configuraciones a una mejor estructura

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { AuthInterceptor } from '././core/interceptors/auth.interceptor';
+import { Auth } from '././core/services/auth.service';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+      deps: [Auth]
+    },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration()
+  ]
 };
