367b8fb9e42ddf95cb6361a963b59bf14a7d6e55
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 23:19:01 -0700

    feat: Agregar interceptor de autenticación
    Agrega un nuevo interceptor de autenticación para agregar el token de autenticación a las solicitudes HTTP salientes.

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Auth } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req);
+};
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  
+  constructor(
+    private auth: Auth
+  ) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req)
+  }
+}
