4a5e94e620aff7ffd80be88d45d1df9e1b287cb3
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 23:19:33 -0700

    feat: Agregar servicio de autenticación
    Se agregó el servicio de autenticación para manejar el inicio de sesión, el almacenamiento del token de autenticación y la verificación del estado de autenticación del usuario. También se implementó la funcionalidad de cierre de sesión.
    
    Closes #123

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Credentials } from '../../core/interfaces/credentials';
+import { map } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class Auth {
+
+    private LOGIN_URL = 'http://localhost:8080/api/auth/login'
+    private TOKEN_KEY = 'token';
+
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) { }
+
+    login(creds: Credentials) {
+        return this.http.post(this.LOGIN_URL, creds, {
+            observe: 'response'
+        }).pipe(map((response: HttpResponse<any>) => {
+            const body = response.body;
+            const headers = response.headers;
+
+            const bearerToken = headers.get('Authorization')!;
+            const token = bearerToken.replace('Bearer ', '');
+
+            localStorage.setItem('token', token);
+            return body;
+        }));
+    }
+
+    setToken(token: string): void {
+        localStorage.setItem(this.TOKEN_KEY, token);
+    }
+
+    getToken(): string | null {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem(this.TOKEN_KEY);
+        } else {
+            return null;
+        }
+    }
+
+    isAuthenticated(): boolean {
+        const token = this.getToken();
+        if (!token) {
+            return false;
+        }
+
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const exp = payload.exp * 1000;
+        return Date.now() < exp;
+    }
+
+    logout(): void {
+        localStorage.removeItem(this.TOKEN_KEY);
+        this.router.navigate(['/login']);
+    }
+}
