8720ad4ecf53a6d54a43dec5d69561b79c266fbb
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 23:18:25 -0700

    feat: Agregar guardia de autenticación

---
+++ src/app/core/helpers/prevent.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const PreventGuard: CanActivateFn = (route, state) =>{
+  const authService = inject(Auth);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return router.navigate(['']);
+  } else {
+    return true;
+  }
+}
