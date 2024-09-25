8dd4575be2ac5e53cf7839ea43cf89616623860e
erneto13 <erneto.amaral13@gmail.com>
2024-09-24 23:16:47 -0700

    feat: Agregar rutas de autenticación y dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './business/auth/login/login.component';
+import { ContactsComponent } from './business/contacts/contacts.component';
+import { AuthGuard } from '././core/helpers/auth.guard';
+import { PreventGuard } from '././core/helpers/prevent.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Auth Paths
+    { path: 'login', component: LoginComponent, canActivate: [PreventGuard] },  // Login route
+    { path: 'dashboard', component: ContactsComponent, canActivate: [AuthGuard] }, // Login route
+
+    // Dashboard Paths
+]
+
+export class AppRoutingModule { }
