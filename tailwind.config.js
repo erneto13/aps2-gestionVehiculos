8c0735e2481203109e4d130851a14dade62bc469
erneto13 <erneto.amaral13@gmail.com>
2024-10-21 01:32:55 -0700

    minor: cambios menores sin importancia

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sidebargray: '#151921',
+        divider: '#272A32',
+        customgreen: '#DDFFBE',
+        customgray: '#7A7B83',
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
