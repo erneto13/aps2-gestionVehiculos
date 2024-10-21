4724ccf0118309d00bf79816e99d4ff05493c05c
erneto13 <erneto.amaral13@gmail.com>
2024-10-21 01:52:47 -0700

    feat: lógica para el cierre de sesión

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { HeaderComponent } from "../header/header.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export default class LayoutComponent {
 
 }
