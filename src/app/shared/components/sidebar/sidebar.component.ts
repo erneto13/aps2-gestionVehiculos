c63882b3e1dca11452ec73a2695b97d736d71e33
erneto13 <erneto.amaral13@gmail.com>
2024-10-21 01:54:35 -0700

    feat: agregar lógica de cierre de sesión en el componente de barra lateral

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth } from '../../../core/services/auth.service';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  constructor(private auth: Auth) {}
+
+  logout(): void {
+    this.auth.logout();
+  }
 
 }
