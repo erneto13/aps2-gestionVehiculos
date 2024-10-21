938694eefa1c47c4007ec513c58c8bbd5a0f37ad
erneto13 <erneto.amaral13@gmail.com>
2024-10-21 01:49:44 -0700

    update: se agregó la lógica del layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,57 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './business/auth/login/login.component';
-import { ContactsComponent } from './business/contacts/contacts.component';
-import { AuthGuard } from '././core/helpers/auth.guard';
+//import { AuthGuard } from '././core/helpers/auth.guard';
 import { PreventGuard } from '././core/helpers/prevent.guard';
+import LayoutComponent from './shared/components/layout/layout.component';
+import { AuthGuard } from './core/helpers/auth.guard';
+
 
 export const routes: Routes = [
     // Auth Paths
     { path: 'login', component: LoginComponent, canActivate: [PreventGuard] },  // Login route
-    { path: 'dashboard', component: ContactsComponent, canActivate: [AuthGuard] }, // Login route
 
     // Dashboard Paths
+    { path: 'sidebar', component: LayoutComponent },
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.component')
+            },
+            {
+                path: 'vehiculos',
+                loadComponent: () => import('./business/vehicles/vehicles.component')
+            },            {
+                path: 'asuntos',
+                loadComponent: () => import('./business/issues/issues.component')
+            },
+            {
+                path: 'recordatorios',
+                loadComponent: () => import('./business/reminders/reminders.component')
+            },            {
+                path: 'contactos',
+                loadComponent: () => import('./business/contacts/contacts.component')
+            },
+            {
+                path: 'rutas',
+                loadComponent: () => import('./business/routes/routes.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+
+    // Otherwise redirect to dashboard
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
 ]
 
 export class AppRoutingModule { }
