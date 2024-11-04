20eddba0db6909047d29dc5dcb65304060aab512
erneto13 <erneto.amaral13@gmail.com>
2024-11-03 19:27:24 -0700

    new: se agregó la verificación de autenticación y la funcionalidad de refresco automático del token en el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Auth } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(private authService: Auth) {
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.authService.autoRefreshToken()
+    }
+  }
 }
