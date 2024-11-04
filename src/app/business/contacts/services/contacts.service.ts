5c2703f3a996487864d19e56e91f1626f1a135a6
erneto13 <erneto.amaral13@gmail.com>
2024-11-03 19:26:16 -0700

    new: se agregaron los metodos CRUD en el servicio de contactos

--- src/app/business/contacts/services/contacts.service.ts
+++ src/app/business/contacts/services/contacts.service.ts
@@ -12,7 +12,20 @@ export class ContactsService {
     private http: HttpClient
   ) { }
 
-  getContacts(): Observable<Contacts[]> {
-    return this.http.get<Contacts[]>('http://localhost:8080/api/contacts');
+  addContact(): Observable<Contacts> {
+    return this.http.post<Contacts>('http://localhost:8080/api/v1/contact', {});
   }
+
+  deleteContact(): Observable<Contacts> {
+    return this.http.delete<Contacts>('http://localhost:8080/api/v1/contact');
+  }
+
+  updateContact(): Observable<Contacts> {
+    return this.http.put<Contacts>('http://localhost:8080/api/v1/contact', {});
+  }
+
+  getContacts(): Observable<Contacts[]> {
+    return this.http.get<Contacts[]>('http://localhost:8080/api/v1/contact-list');
+  }
+
 }
