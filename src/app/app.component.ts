59cac2aba64ebaee2129294655db6caf75b04806
erneto13 <erneto.amaral13@gmail.com>
2024-11-06 22:21:41 -0700

    new: funcionalidad con flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Auth } from './core/services/auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService: Auth) {
-  }
+  constructor(private authService: Auth, @Inject(PLATFORM_ID) private platformId: Object) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      // Importa flowbite solo en el cliente
+      const { initFlowbite } = await import('flowbite');
+      initFlowbite();  // Solo se ejecutará en el navegador
+    }
     if (this.authService.isAuthenticated()) {
-      this.authService.autoRefreshToken()
+      this.authService.autoRefreshToken();
     }
   }
 }
