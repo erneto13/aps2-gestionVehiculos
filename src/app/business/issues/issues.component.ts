111943edbd3c92b58b636554a9a1573bf3a2adcb
erneto13 <erneto.amaral13@gmail.com>
2024-11-06 22:27:47 -0700

    restructuring: trabajando en el componente de asuntos

--- src/app/business/issues/issues.component.ts
+++ src/app/business/issues/issues.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IssuesSidebarComponent } from "./shared/issues-sidebar/issues-sidebar.component";
+import { IssuesHeaderComponent } from "./shared/issues-header/issues-header.component";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-issues',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, IssuesSidebarComponent, 
+    IssuesHeaderComponent, RouterOutlet],
   templateUrl: './issues.component.html',
   styleUrl: './issues.component.css'
 })
 export default class IssuesComponent {
-
 }
