7904d46cd7f8b7dae43b46e8c790713c42eddd31
erneto13 <erneto.amaral13@gmail.com>
2024-11-06 22:23:59 -0700

    new: componentes para evitar carga lenta

---
+++ src/app/business/issues/shared/issues-header/issues-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './issues-header.component.html',
+  styleUrl: './issues-header.component.css'
+})
+export class IssuesHeaderComponent {
+}
---
+++ src/app/business/issues/shared/issues-sidebar/issues-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './issues-sidebar.component.html',
+  styleUrl: './issues-sidebar.component.css'
+})
+export class IssuesSidebarComponent {
+}
