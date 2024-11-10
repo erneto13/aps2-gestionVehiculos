59aba9b7dc1bc94c184201e0d4376d2644c1edcf
erneto13 <erneto.amaral13@gmail.com>
2024-11-10 04:48:51 -0700

    test: probando el primeng

--- src/app/business/dashboard/dashboard.component.ts
+++ src/app/business/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { KnobModule } from 'primeng/knob';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, KnobModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export default class DashboardComponent {
+  value!: number;
 
 }
