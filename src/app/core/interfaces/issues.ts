f1e291aa98f3049ad23bc3564af8ece0e0f1fec3
erneto13 <erneto.amaral13@gmail.com>
2024-11-11 20:15:40 -0700

    feature: agregar nuevas interfaces y enumeraciones para la gestión de problemas

--- src/app/core/interfaces/issues.ts
+++ src/app/core/interfaces/issues.ts
@@ -6,7 +6,27 @@ export interface Issues {
     status: string;
     evidence: string[];
     comments: string;
-    reportDate: string;
+    reportedBy: string;
+    reportedDate: string;
     resolvedBy: string | null;
     resolvedDate: string | null;
+}
+
+export interface NewIssues {
+    title: string;
+    description: string;
+    issue_type: string;
+    status: string;
+    evidence: string[];
+    reportedBy: string;
+    reportedDate: string;
+}
+
+export enum IssueType {
+    VEHICULOS,
+    ASIGNACION_DE_VEHICULOS,
+    INSPECCIONES,
+    SERVICIOS,
+    AUTO_PARTES,
+    COMBUSTIBLE
 }
