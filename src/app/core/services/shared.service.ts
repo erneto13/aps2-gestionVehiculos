bce6d50b23d777b60c94a2da0d128e160400db84
erneto13 <erneto.amaral13@gmail.com>
2024-11-24 03:23:23 -0700

    feature: agregar función para convertir datetime-local a timestamp MySQL

--- src/app/core/services/shared.service.ts
+++ src/app/core/services/shared.service.ts
@@ -29,4 +29,22 @@ export class SharedService {
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
+
+  /*
+  * Convierte un datetime-local a un timestamp MySQL
+  * @param datetimeLocal string en formato 'YYYY-MM-DDTHH:mm'
+  * @returns string en formato 'YYYY-MM-DD HH:mm:ss'
+  * Ejemplo: '2024-11-06T10:15' -> '2024-11-06 10:15:00'
+  */
+  convertToMySQLTimestamp(datetimeLocal: string): string {
+    const date = new Date(datetimeLocal);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Mes empieza en 0
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
 }
