140e260677bbc3c6ebe95a5f446c23d1e6d36556
erneto13 <erneto.amaral13@gmail.com>
2024-11-23 23:36:49 -0700

    feature: agregar componente de reservas con plantilla básica

---
+++ src/app/business/vehicles/shared/vehicle-details/header-options/bookings/bookings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookings.component.html',
+})
+export default class BookingsComponent {
+
+}
