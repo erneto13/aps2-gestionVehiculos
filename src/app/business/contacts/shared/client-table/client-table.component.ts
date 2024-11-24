e6b6ccf18ecd12ba3997022802832d293bc06d35
erneto13 <erneto.amaral13@gmail.com>
2024-11-24 06:21:26 -0700

    feature: agregar componente ClientTable para mostrar la lista de contactos con opciones de eliminación

---
+++ src/app/business/contacts/shared/client-table/client-table.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-client-table',
+    standalone: true,
+    imports: [CommonModule],
+    template: `
+    <div class="overflow-x-auto rounded-lg border border-gray-200">
+      <div class="overflow-y-auto max-h-[400px]">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead class="bg-gray-50">
+            <tr>
+              @for (column of columns; track $index) {
+              <th scope="col"
+                  class="sticky top-0 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-gray-50 shadow"
+                  style="white-space: nowrap; width: 200px;"> 
+                  {{ column }}
+              </th>
+              }
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            @if (contacts.length > 0) {
+            @for (contact of contacts; track $index) {
+            <tr class="hover:bg-gray-100 cursor-pointer">
+              <td class="px-6 py-4 text-sm font-medium text-slate-900" style="width: 200px; white-space: nowrap;">
+                  {{ contact.name }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 200px; white-space: wrap;">
+                  {{ contact.company }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 300px; max-height: 100px; overflow-y: auto;">
+                  {{ contact.phone }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 300px; max-height: 100px; overflow-y: auto;">
+                  {{ contact.email }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 300px; max-height: 100px; overflow-y: auto;">
+                  {{ contact.service }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 200px; white-space: wrap;">
+                <span [class]="'inline-flex px-2 py-1 text-xs font-semibold rounded-full ' + 
+                (contact.status === 'Activo' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800')">
+                  {{ contact.status }}
+                </span>
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" style="width: 150px;">
+                  <button
+                      class="bg-red-500 hover:bg-red-600 text-white font-medium py-1 px-4 rounded inline-flex items-center"
+                      (click)="deleteContact(contact.contact_id!)">
+                      Eliminar
+                  </button>
+              </td>
+            </tr>
+            }
+            } @else {
+            <tr>
+              <td colspan="7" class="px-6 py-4 text-center text-gray-500">
+                <div class="flex flex-col items-center">
+                  <i class="pi pi-info-circle text-4xl text-gray-400 mb-2"></i>
+                  <p class="text-xl font-medium text-gray-600">No hay datos disponibles</p>
+                  <p class="text-sm text-gray-400">No se ha encontrado ningún cliente</p>
+                </div>
+              </td>
+            </tr>
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `,
+})
+export class ClientTableComponent {
+    @Input() contacts: any[] = [];
+    @Input() columns: string[] = [];
+    @Output() delete = new EventEmitter<number>();
+
+    deleteContact(id: number): void {
+        this.delete.emit(id);
+    }
+}
