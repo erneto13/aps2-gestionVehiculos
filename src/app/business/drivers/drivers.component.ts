2def7a6c4cfa4422d60075bbdb37804153bc740c
erneto13 <erneto.amaral13@gmail.com>
2024-11-23 23:34:50 -0700

    new: nuevos componentes

---
+++ src/app/business/drivers/drivers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drivers',
+  standalone: true,
+  imports: [],
+  templateUrl: './drivers.component.html',
+})
+export default class DriversComponent {
+
+}
