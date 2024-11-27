91a08ef428846e82a5c0ed98f39d98c52d08a964
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:50:26 -0700

    feature: se agregaron componentes de encabezado, pie de página, información y generación de gemini para la landing-page

---
+++ src/app/shared/components/landing-page/gemini-generate/gemini-generate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gemini-generate',
+  standalone: true,
+  imports: [],
+  templateUrl: './gemini-generate.component.html',
+  styleUrl: './gemini-generate.component.css'
+})
+export class GeminiGenerateComponent {
+
+}
---
+++ src/app/shared/components/landing-page/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-page-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styles: ''
+})
+export default class LayoutLandingPage {
+
+}
