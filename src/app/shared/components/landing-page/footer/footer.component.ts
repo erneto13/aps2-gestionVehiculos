b4bf4f19d176e22dbb8092f674c91526f84da605
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 11:32:49 -0700

    feature: se rediseñó el pie de página con nueva estructura, contenido y estilos

--- src/app/shared/components/landing-page/footer/footer.component.ts
+++ src/app/shared/components/landing-page/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer-landing',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterLandingPage {
 
 }
