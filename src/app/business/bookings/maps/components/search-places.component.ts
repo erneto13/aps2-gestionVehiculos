718cfd4104837f99d9b7e409248085a4970c5b8f
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:46:15 -0700

    feature: se agregó el componente de búsqueda de lugares con debounce

---
+++ src/app/business/bookings/maps/components/search-places.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { SearchResultsPlaces } from './search-results.component';
+import { PlacesService } from '../services';
+
+@Component({
+    standalone: true,
+    imports: [SearchResultsPlaces],
+    selector: 'app-search-places',
+    templateUrl: 'search-places.component.html'
+})
+
+export class SearchPlacesInput {
+    private debounceTimer?: NodeJS.Timeout;
+
+    constructor(private places: PlacesService) { }
+
+    onQueryChange(query: string = '') {
+
+        if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+
+        this.debounceTimer = setTimeout(() => {
+            this.places.getPlaces(query)
+        }, 500)
+    }
+
+}
