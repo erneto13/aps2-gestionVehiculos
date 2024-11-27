21e60b1f322b95e22d8c13206d80b0fff8d1678e
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:44:44 -0700

    feature: se agregó el componente que hará funcionar la sección de bookings

--- src/app/business/bookings/bookings.component.ts
+++ src/app/business/bookings/bookings.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { BookingsListComponent } from './shared/bookings-list/bookings-list.component';
+import { PlacesService } from './maps/services';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [],
+  imports: [BookingsListComponent],
   templateUrl: './bookings.component.html',
 })
 export default class BookingsComponent {
+
+  constructor(private placesService: PlacesService) { }
 
 }
