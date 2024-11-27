f45d133115e1287b1e7b06ebd1fd38dba86b4eb5
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:48:31 -0700

    feature: se agregó la lógica para crear y gestionar marcadores y linestring en el mapa

--- src/app/business/bookings/maps/services/map.service.ts
+++ src/app/business/bookings/maps/services/map.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../../../../core/interfaces/places';
+import { DirectionsApiClient } from '../api/directionsApiClient';
+import { DirectionsResponse, Route } from '../../../../core/interfaces/directions';
 
 @Injectable({ providedIn: 'root' })
 
 export class MapService {
 
     private map?: Map;
+    private markers: Marker[] = [];
+
     get isMapReady(): boolean {
         return !!this.map;
     }
+
+    constructor(private directionsApi: DirectionsApiClient) { }
 
     setMap(map: Map) {
         this.map = map;
@@ -19,7 +26,102 @@ export class MapService {
         this.map?.flyTo({
             center: lngLat,
             zoom: 14,
-            essential: true
+        });
+    }
+
+    createMarkersFromPlaces(places: Feature[], userLocation: [number, number]) {
+        if (!this.map) throw new Error('Map not ready');
+
+        this.markers.forEach(marker => marker.remove());
+        const newMarkers = [];
+
+        for (const place of places) {
+            const [lng, lat] = place.geometry.coordinates;
+            const popup = new Popup()
+                .setHTML(`
+                    <h3>${place.properties.name}</h3>
+                    <p>${place.properties.full_address}</p>
+                    `);
+
+            const newMarker = new Marker()
+                .setLngLat([lng, lat])
+                .setPopup(popup)
+                .addTo(this.map);
+
+            newMarkers.push(newMarker);
+        }
+        this.markers = newMarkers;
+
+        if (places.length === 0) return;
+
+        // Zoom inicial para los límites del mapa
+        const bounds = new LngLatBounds();
+        newMarkers.forEach(market => bounds.extend(market.getLngLat()));
+        bounds.extend(userLocation);
+
+        this.map.fitBounds(bounds, {
+            padding: 120,
+        });
+    }
+
+    getRouteBetweenPoints(origin: [number, number], destination: [number, number]) {
+        this.directionsApi.get<DirectionsResponse>(`/${origin.join(',')};${destination.join(',')}`)
+            .subscribe(response => {
+                this.drawLineString(response.routes[0]);
+            });
+    }
+
+    private drawLineString(route: Route) {
+        if (!this.map) throw new Error('Map not ready');
+
+        const coords = route.geometry.coordinates;
+
+        const bounds = new LngLatBounds();
+        coords.forEach(([lng, lat]) => {
+            bounds.extend([lng, lat]);
+        });
+        this.map?.fitBounds(bounds, {
+            padding: 120,
+        })
+
+        // polyline
+        const sourceData: AnySourceData = {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: coords
+                        }
+                    }
+                ]
+            }
+        }
+
+        // TODO: limpiar line string previa
+        if (this.map.getLayer('RouteString')) {
+            this.map.removeLayer('RouteString');
+            this.map.removeSource('RouteString');
+        }
+
+
+        this.map.addSource('RouteString', sourceData)
+        this.map.addLayer({
+            id: 'RouteString',
+            type: 'line',
+            source: 'RouteString',
+            layout: {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            paint: {
+                'line-color': 'black',
+                'line-width': 3
+            }
         });
     }
 }
