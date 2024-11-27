927182eb4906f7498a764f3dd9884001bd54d12e
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:46:26 -0700

    feature: se agregó el componente de resultados de búsqueda para lugares

---
+++ src/app/business/bookings/maps/components/search-results.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { MapService, PlacesService } from '../services';
+import { Feature } from '../../../../core/interfaces/places';
+
+@Component({
+    standalone: true,
+    imports: [],
+    selector: 'app-search-results',
+    templateUrl: 'search-results.component.html'
+})
+
+export class SearchResultsPlaces {
+    constructor(private placesService: PlacesService, private mapService: MapService) { }
+
+    get isLoadingPlaces() {
+        return this.placesService.isLoadingPlaces;
+    }
+
+    get places() {
+        return this.placesService.places;
+    }
+
+    flyTo(place: Feature) {
+        const [lng, lat] = place.geometry.coordinates;
+        this.mapService.flyTo([lng, lat]);
+    }
+
+    getDirections(place: Feature) {
+        if (!this.placesService.useLocation) throw new Error('No location to get directions from');
+
+        this.placesService.deletePlaces();
+
+        const origin = this.placesService.useLocation;
+        const destination = place.geometry.coordinates as [number, number];
+
+        this.mapService.getRouteBetweenPoints(origin, destination);
+    }
+
+}
