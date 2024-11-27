eb1f3cc5ce537863fee772d4052fdcda62fb2741
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:48:51 -0700

    feature: se agregó el servicio de reservas con métodos para gestionar reservas

---
+++ src/app/business/bookings/services/bookings.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Booking, BookingResponse } from '../../../core/interfaces/booking';
+
+@Injectable({ providedIn: 'root' })
+
+export class BookingsService {
+
+    apiUrl = 'http://localhost:8080/api/v1/bookings';
+    constructor(private http: HttpClient) { }
+
+    // Obtener todas las reservas
+    getAllBookings(): Observable<Booking[]> {
+        return this.http.get<Booking[]>(`${this.apiUrl}/bookings-list`);
+    }
+
+    // Obtener una reserva por ID
+    getBookingById(id: number): Observable<Booking> {
+        return this.http.get<Booking>(`${this.apiUrl}/${id}`);
+    }
+
+    // Obtener reservas asignadas a un conductor
+    getBookingsByDriver(driverId: number): Observable<Booking[]> {
+        return this.http.get<Booking[]>(`${this.apiUrl}/driver/${driverId}`);
+    }
+
+    // Crear una nueva reserva
+    createBooking(booking: BookingResponse): Observable<BookingResponse> {
+        return this.http.post<BookingResponse>(`${this.apiUrl}`, booking);
+    }
+
+    // Actualizar una reserva existente
+    updateBooking(id: number, booking: Booking): Observable<Booking> {
+        return this.http.put<Booking>(`${this.apiUrl}/${id}`, booking);
+    }
+
+    // Eliminar una reserva
+    deleteBooking(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+
+}
