a32c018b9c4d9134d3540a56330c2e656cde2343
erneto13 <erneto.amaral13@gmail.com>
2024-11-27 00:52:53 -0700

    feature: se agregó la clave API al entorno de configuración

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiKey: 'pk.eyJ1IjoiZXJuZXRvMTMiLCJhIjoiY20zd2I2MW10MTFueTJycHUxdXl0Y3ZzZiJ9.fUMW24vzqzc9Ul9vuXYUtA',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiZXJuZXRvMTMiLCJhIjoiY20zd2I2MW10MTFueTJycHUxdXl0Y3ZzZiJ9.fUMW24vzqzc9Ul9vuXYUtA'
   }
