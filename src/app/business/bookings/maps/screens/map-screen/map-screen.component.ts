2948aa28ad721485d847bca8a39b3e8cbdde4176
erneto13 <erneto.amaral13@gmail.com>
2024-11-28 16:33:02 -0700

    update: integrar componente de búsqueda de lugares en la pantalla del mapa

--- src/app/business/bookings/maps/screens/map-screen/map-screen.component.ts
+++ src/app/business/bookings/maps/screens/map-screen/map-screen.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PlacesService } from '../../services';
 import { LoadingComponent } from '../../components/loading/loading.component';
 import { MapViewComponent } from '../../components/map-view/map-view.component';
+import { SearchPlacesInput } from '../../components/search-places.component';
 
 @Component({
   selector: 'app-map-screen',
   standalone: true,
-  imports: [LoadingComponent, MapViewComponent],
+  imports: [LoadingComponent, MapViewComponent, SearchPlacesInput],
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })
