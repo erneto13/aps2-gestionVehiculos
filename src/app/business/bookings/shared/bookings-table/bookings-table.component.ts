828e845c57ca2375ac14d0badf7b3c0935d28422
erneto13 <erneto.amaral13@gmail.com>
2024-11-29 04:21:04 -0700

    update: agregar diálogo modal para mostrar detalles de la reserva en la tabla de reservas

--- src/app/business/bookings/shared/bookings-table/bookings-table.component.ts
+++ src/app/business/bookings/shared/bookings-table/bookings-table.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { BookingResponse } from '../../../../core/interfaces/booking';
+import { Booking, BookingResponse } from '../../../../core/interfaces/booking';
+import { DialogModule } from 'primeng/dialog';
+import { BookingDetailsComponent } from '../booking-details/booking-details.component';
 
 @Component({
   selector: 'app-bookings-table',
   standalone: true,
-  imports: [],
+  imports: [DialogModule, BookingDetailsComponent],
   templateUrl: './bookings-table.component.html',
 })
 export class BookingsTableComponent {
@@ -12,11 +14,21 @@ export class BookingsTableComponent {
   tableHeaders: string[] = ['Vehículo', 'Conductor', 'Cliente', 'Fecha', 'Status', 'Destino', 'Acciones'];
   @Output() delete = new EventEmitter<number>();
 
+  visibility: boolean = false;
+  selectedBooking: BookingResponse | null = null;
+
   deleteBooking(id: number): void {
     this.delete.emit(id);
   }
 
-  viewDetails(id: number): void {
-    
+  onChange() {
+    this.visibility = !this.visibility;
+  }
+
+  viewDetails(bookingId: number) {
+    this.selectedBooking = this.bookings.find(booking => booking.bookings_id === bookingId) || null;
+
+    // Abre el diálogo
+    this.visibility = true;
   }
 }
