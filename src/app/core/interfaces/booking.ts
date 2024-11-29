82ea875a1e4b4edace1bfda6e8e9ff685668b045
erneto13 <erneto.amaral13@gmail.com>
2024-11-29 00:36:40 -0700

    update: modificar la tabla de reservas para mostrar información detallada del conductor, vehículo y contacto

--- src/app/core/interfaces/booking.ts
+++ src/app/core/interfaces/booking.ts
@@ -1,8 +1,12 @@
+import { Contacts } from "./contacts";
+import { Drivers } from "./drivers";
+import { Vehicle } from "./vehicle";
+
 export interface BookingResponse {
     bookings_id: number,
-    vehicle_id: number,
-    driverId: number,
-    contact_id: number,
+    vehicle: Vehicle,
+    driver: Drivers,
+    contact: Contacts,
     start_date: string,
     end_date: string,
     status: string,
