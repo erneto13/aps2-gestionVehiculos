fee063acf00785a2e6199792a53a078fd33c8b2c
erneto13 <erneto.amaral13@gmail.com>
2024-11-29 19:10:41 -0700

    update: mejorar la tabla de reservas para mostrar la matrícula del vehículo de forma más clara y formatear las fechas

--- src/app/business/bookings/shared/bookings-table/bookings-table.component.ts
+++ src/app/business/bookings/shared/bookings-table/bookings-table.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Booking, BookingResponse } from '../../../../core/interfaces/booking';
 import { DialogModule } from 'primeng/dialog';
 import { BookingDetailsComponent } from '../booking-details/booking-details.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-bookings-table',
   standalone: true,
-  imports: [DialogModule, BookingDetailsComponent],
+  imports: [DialogModule, BookingDetailsComponent, DatePipe],
   templateUrl: './bookings-table.component.html',
 })
 export class BookingsTableComponent {
