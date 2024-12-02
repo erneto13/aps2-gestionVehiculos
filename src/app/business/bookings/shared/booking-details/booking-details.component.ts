fbe19665c7d10e5118237f497d42214a731be015
erneto13 <erneto.amaral13@gmail.com>
2024-12-01 17:16:55 -0700

    update: agregar detalles de reserva con componente de mapa y coordenadas de origen y destino

--- src/app/business/bookings/shared/booking-details/booking-details.component.ts
+++ src/app/business/bookings/shared/booking-details/booking-details.component.ts
@@ -1,14 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Booking, BookingResponse } from '../../../../core/interfaces/booking';
+import { BookingResponse } from '../../../../core/interfaces/booking';
+import { MapBooking } from '../../maps/components/map-booking.component';
 
 @Component({
   selector: 'app-booking-details',
   standalone: true,
-  imports: [],
+  imports: [MapBooking],
   templateUrl: './booking-details.component.html',
 })
 export class BookingDetailsComponent implements OnInit {
   @Input() booking: BookingResponse | null = null;
+
+  get origin(): [number, number] {
+    if (this.booking) {
+      return [this.booking.origin_lng, this.booking.origin_lat];
+    }
+    return [0, 0]; // Coordenadas predeterminadas
+  }
+
+  get destination(): [number, number] {
+    if (this.booking) {
+      return [this.booking.destination_lng, this.booking.destination_lat];
+    }
+    return [0, 0]; // Coordenadas predeterminadas
+  }
 
   ngOnInit(): void {
     this.loadDetails();
