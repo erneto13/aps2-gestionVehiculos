f63e1eb0a41692d7155f29c2f95c61093a2d4af6
erneto13 <erneto.amaral13@gmail.com>
2024-12-02 09:22:17 -0700

    update: agregar componente de reservas y registro de combustible en bookings.component

--- src/app/business/vehicles/shared/vehicle-details/header-options/bookings/bookings.component.ts
+++ src/app/business/vehicles/shared/vehicle-details/header-options/bookings/bookings.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { Vehicle } from '../../../../../../core/interfaces/vehicle';
+import { BookingResponse } from '../../../../../../core/interfaces/booking';
+import { VehicleApiService } from '../../../../services/vehicle-api.service';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { BookingsService } from '../../../../../bookings/services/bookings.service';
+import { FuelRecordResponse } from '../../../../../../core/interfaces/fuel_records';
+import { DialogModule } from 'primeng/dialog';
+import { FuelRecordFormComponent } from './fuel-record-form/fuel-record-form.component';
+import { BookingsTableComponent } from './bookings-table/bookings-table.component';
+import { FuelRecordsTableComponent } from './fuel-records-table/fuel-records-table.component';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CurrencyPipe,
+    DialogModule, FuelRecordFormComponent,
+    BookingsTableComponent, FuelRecordsTableComponent],
   templateUrl: './bookings.component.html',
 })
 export default class BookingsComponent {
+  vehicle!: Vehicle;
+  bookings: BookingResponse[] = [];
+  fuelRecord: FuelRecordResponse[] = [];
 
+  visible: boolean = false;
+
+  constructor(
+    private bookingsService: BookingsService,
+    private vehicleService: VehicleApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.vehicleService.selectedVehicle$.subscribe((vehicle) => {
+      this.vehicle = vehicle;
+      this.loadBookings();
+      this.loadFuelRecords();
+    });
+  }
+
+  loadBookings(): void {
+    this.bookingsService.getBookingsForVehicle(this.vehicle.vehicle_id)
+      .subscribe((bookings) => {
+        this.bookings = bookings;
+      });
+  }
+
+  loadFuelRecords(): void {
+    this.vehicleService.getRecordsByVehicleId(this.vehicle.vehicle_id)
+      .subscribe((fuelRecord) => {
+        this.fuelRecord = fuelRecord;
+        this.visible = false;
+      });
+  }
+
+  onChange(): void {
+    this.visible = !this.visible;
+    console.log(this.visible)
+  }
 }
