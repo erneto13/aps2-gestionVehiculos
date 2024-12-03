9be0c0cb321b556fe1059f6d0332da4bf26694f6
erneto13 <erneto.amaral13@gmail.com>
2024-12-03 06:14:12 -0700

    update: agregar componente de gráfico de gastos en el tablero

---
+++ src/app/business/dashboard/shared/expense-chart/expense-chart.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { ChartData, ChartOptions } from '../../../../core/interfaces/dashboard';
+
+@Component({
+  selector: 'app-expense-chart',
+  standalone: true,
+  imports: [CommonModule, ChartModule, CurrencyPipe],
+  templateUrl: './expense-chart.component.html',
+})
+export class ExpenseChartComponent {
+  @Input() totalExpense!: number;
+  @Input() title: string = 'Total de dinero gastado en combustible.';
+  @Input() percentageChange: number = 0;
+  @Input() chartData!: ChartData;
+  @Input() chartOptions!: ChartOptions;
+}
