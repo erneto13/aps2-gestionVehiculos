454a99355e926144cf2b253c8d17a8b39a6d4ad3
erneto13 <erneto.amaral13@gmail.com>
2024-12-04 01:36:34 -0700

    refactor: se movió toda la lógica e interfaz a un nuevo componente para evitar sobrecarga y mejorar la mantebilidad

---
+++ src/app/business/issues/shared/issues-form/issues-form.component.ts
@@ -0,0 +1,165 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MediaService } from '../../../../core/services/media.service';
+import { IssuesService } from '../../services/issues.service';
+import { ToastService } from '../../../../core/services/toast.service';
+import { SharedService } from '../../../../core/services/shared.service';
+import { Issue, IssuesType } from '../../../../core/interfaces/issues';
+import { Auth } from '../../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-issues-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './issues-form.component.html',
+})
+export class IssuesFormComponent implements OnInit {
+  @Output() issueCreated = new EventEmitter<void>();
+  issueForm!: FormGroup;
+
+  uploadedFiles: File[] = [];
+  previewUrls: string[] = []; 
+  driverName: string | null = null;
+
+  issuesType = Object.values(IssuesType);
+
+  constructor(
+    private auth: Auth,
+    private fb: FormBuilder,
+    private mediaService: MediaService,
+    private issueService: IssuesService,
+    private toastService: ToastService,
+    private sharedService: SharedService
+  ) {
+    this.issueForm = this.fb.group({
+      title: ['', [Validators.required, Validators.nullValidator]],
+      description: ['', [Validators.required, Validators.nullValidator]],
+      selectedIssueType: ['', Validators.required],
+      evidenceValidation: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.getDriverName();
+  }
+
+  onSubmit(): void {
+    if (this.issueForm.invalid) {
+      this.toastService.showToast(
+        'Error',
+        'Por favor, rellene todos los campos',
+        'error'
+      );
+      return;
+    }
+
+    if (this.uploadedFiles.length > 0) {
+      const formData = new FormData();
+      this.uploadedFiles.forEach((file) => {
+        formData.append('files', file);
+      });
+
+      this.mediaService.uploadFile(formData).subscribe({
+        next: (response) => {
+          if (response.urls && Array.isArray(response.urls)) {
+            const rawDate = this.sharedService.getMySQLTimestamp();
+
+            const issue: Issue = {
+              title: this.issueForm.value.title,
+              description: this.issueForm.value.description,
+              issue_type: this.issueForm.value.selectedIssueType,
+              status: 'PENDING',
+              evidence: response.urls,
+              reportedBy: this.driverName ?? 'Administrador',
+              reportDate: rawDate,
+            };
+
+            this.createIssue(issue); 
+          }
+        },
+        error: () => {
+          this.toastService.showToast(
+            'Error al subir imágenes',
+            'Hubo un problema al cargar las imágenes. Inténtelo de nuevo.',
+            'error'
+          );
+        }
+      });
+    } else {
+      this.toastService.showToast(
+        'Error',
+        'Debe seleccionar al menos una imagen antes de enviar el formulario.',
+        'error'
+      );
+    }
+  }
+
+  createIssue(issue: Issue) {
+    this.issueService.pushIssue(issue).subscribe({
+      next: () => {
+        this.toastService.showToast(
+          'Asunto creado',
+          'Se ha creado el asunto correctamente',
+          'success'
+        );
+        this.issueCreated.emit();
+        this.issueForm.reset();
+        this.uploadedFiles = []; 
+        this.previewUrls = []; 
+      },
+      error: () => {
+        this.toastService.showToast(
+          'Ha ocurrido un error',
+          'Hubo un problema al crear el asunto.',
+          'error'
+        );
+      }
+    });
+  }
+
+  upload(event: any) {
+    const files: FileList = event.target.files;
+
+    if (files && files.length > 0) {
+      Array.from(files).forEach((file: File) => {
+        this.uploadedFiles.push(file);
+
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.previewUrls.push(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      });
+
+      this.toastService.showToast(
+        'Archivos cargados',
+        'Las imágenes se han seleccionado correctamente.',
+        'success'
+      );
+
+      this.issueForm.patchValue({
+        evidenceValidation: 'valid'
+      });
+    } else {
+      this.issueForm.patchValue({
+        evidenceValidation: ''
+      });
+    }
+  }
+
+  removeFile(index: number): void {
+    this.uploadedFiles.splice(index, 1);
+    this.previewUrls.splice(index, 1);
+
+    if (this.uploadedFiles.length === 0) {
+      this.issueForm.patchValue({
+        evidenceValidation: ''
+      });
+    }
+  }
+
+  getDriverName(): void {
+    this.driverName = this.auth.getDriverName();
+  }
+}
--- src/app/business/issues/shared/issues-header/issues-header.component.ts
+++ src/app/business/issues/shared/issues-header/issues-header.component.ts
@@ -1,167 +1,28 @@
 // Bodriular
-import { CommonModule, TitleCasePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 // Core & Shared
-import { Auth } from '../../../../core/services/auth.service';
-import { MediaService } from '../../../../core/services/media.service';
-import { ButtonComponent } from "../../../../shared/utils/button/button.component";
-import { IssueType, NewIssues } from '../../../../core/interfaces/issues';
+import { IssuesFormComponent } from '../issues-form/issues-form.component';
 
 // PrimeNG
 import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { IssuesService } from '../../services/issues.service';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-issues-header',
   standalone: true,
-  imports: [TitleCasePipe, ButtonComponent, DialogModule, InputTextareaModule,
-    DropdownModule, CommonModule, ReactiveFormsModule, ToastModule
-  ],
-  providers: [MessageService],
+  imports: [DialogModule, IssuesFormComponent],
   templateUrl: './issues-header.component.html',
-  styleUrl: './issues-header.component.css'
 })
 export class IssuesHeaderComponent implements OnInit {
   @Input() issueType: string = '';
   visible: boolean = false;
 
-  newIssueForm: FormGroup
-  userName: string | null = null;
-  listOfTimes: Date[] = [];
-  uploadedFiles: string[] = [];
-
-  constructor(private fb: FormBuilder,
-    private auth: Auth, private ms: MediaService, private is: IssuesService, private msgs: MessageService
-  ) {
-    this.newIssueForm = this.fb.group({
-      title: ['', [Validators.required, Validators.nullValidator]],
-      description: ['', [Validators.required, Validators.nullValidator]],
-      selectedIssueType: ['', Validators.required],
-      evidenceValidation: ['', [Validators.required]]
-    })
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.auth.user$.subscribe(user => {
-      if (user) {
-        this.userName = user?.name;
-      }
-    })
   }
 
-  pushIssue() {
-    if (this.newIssueForm.valid && this.uploadedFiles.length > 0) {
-      const currentTimestamp = this.getMySQLTimestamp();
-
-      const issueAttributes: NewIssues = {
-        title: this.newIssueForm.value.title,
-        description: this.newIssueForm.value.description,
-        issue_type: this.newIssueForm.value.selectedIssueType,
-        status: 'PENDING',
-        evidence: this.uploadedFiles,
-        reportedBy: this.userName ?? 'Unknown',
-        reportDate: currentTimestamp,
-        resolvedBy: null,
-        resolvedDate: null,
-        comments: null,
-      };
-      this.is.pushIssue(issueAttributes).subscribe({
-        next: (response) => {
-          this.msgs.add({
-            severity: 'success',
-            summary: 'Asunto creado exitosamente',
-            detail: 'Se agregó tu asunto a la lista.',
-          });
-          this.newIssueForm.reset();
-          this.uploadedFiles = [];
-          this.visible = false;
-        },
-        error: (error) => {
-          this.msgs.add({
-            severity: 'error',
-            summary: 'Error al crear asunto',
-            detail: error.message || 'Hubo un problema al crear el asuntp.',
-          });
-        }
-      })
-    }
-  }
-
-  openCreateIssueModal() {
+  onChange() {
     this.visible = true;
   }
-
-  issueTypes = Object.keys(IssueType).filter((key) => isNaN(Number(key)))
-    .map((key) => ({ label: this.titleCase(key), value: key }));
-
-  titleCase(str: string): string {
-    return str.replace(/_/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase());
-  }
-
-  get canSubmit(): boolean {
-    return this.newIssueForm.valid && this.uploadedFiles.length > 0;
-  }
-
-  upload(event: any) {
-    const files: FileList = event.target.files;
-
-    if (files && files.length > 0) {
-      const formData = new FormData();
-
-      Array.from(files).forEach((file: File) => {
-        formData.append('files', file);
-      });
-
-      this.ms.uploadFile(formData).subscribe({
-        next: (response) => {
-          this.msgs.add({
-            severity: 'success',
-            summary: 'Archivos subidos',
-            detail: 'Los archivos fueron subidos exitosamente.',
-          }); if (response.urls && Array.isArray(response.urls)) {
-            this.uploadedFiles = response.urls;
-            this.newIssueForm.patchValue({
-              evidenceValidation: 'valid'
-            });
-          }
-        },
-        error: (error) => {
-          console.error('Error al subir archivos:', error);
-          this.newIssueForm.patchValue({
-            evidenceValidation: ''
-          });
-        }
-      });
-    } else {
-      this.newIssueForm.patchValue({
-        evidenceValidation: ''
-      });
-    }
-  }
-
-
-  /**
-   * Obtiene el timestamp actual en formato MySQL
-   * @returns string en formato 'YYYY-MM-DD HH:mm:ss'
-   * Ejemplo: '2024-11-06 10:15:00'
-   */
-  getMySQLTimestamp(): string {
-    const now = new Date();
-
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  }
-
 }
