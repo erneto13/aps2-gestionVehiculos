ed84516b8f7e940adbf85bff75c903097c5ecff2
erneto13 <erneto.amaral13@gmail.com>
2024-12-05 23:48:38 -0700

    refactor: simplificación del componente IssuesSidebar y eliminación de comentarios innecesarios

--- src/app/business/issues/shared/issues-sidebar/issues-sidebar.component.ts
+++ src/app/business/issues/shared/issues-sidebar/issues-sidebar.component.ts
@@ -12,26 +12,15 @@ import { BadgeModule } from 'primeng/badge';
   styleUrl: './issues-sidebar.component.css'
 })
 export class IssuesSidebarComponent {
+  @Output() selectedIssueType = new EventEmitter<string>();
 
   constructor(private router: Router) { }
 
-  @Output() selectedIssueType = new EventEmitter<string>();
-
-  /*
-  Método para navegar a las diferentes rutas
-  de la opcion seleccionada del Sidebar-Issues.
-  @param issueType: string - Tipo de asunto.
-  */
   onIssueTypeSelect(issueType: string) {
     this.router.navigate([`/asuntos/${issueType}`]);
     this.selectedIssueType.emit(issueType);
   }
 
-  /*
-  Método para navegar a la ruta de los asuntos
-  con un status de cerrado o el seleccionado.
-  @param issueStatus: string - Status del asunto.
-  */
   onIssueStatusSelect(issueStatus: string) {
     this.router.navigate([`/asuntos/${issueStatus}`]);
     this.selectedIssueType.emit(issueStatus);
