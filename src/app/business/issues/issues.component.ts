447f31835291d5c1398adc7df0924313a499e1c4
erneto13 <erneto.amaral13@gmail.com>
2024-12-06 07:11:49 -0700

    feat: se agregó un spinner para indicar la carga de datos en el componente de asuntos

--- src/app/business/issues/issues.component.ts
+++ src/app/business/issues/issues.component.ts
@@ -1,16 +1,20 @@
+// Bodriular
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+// Core
 import { IssuesSidebarComponent } from "./shared/issues-sidebar/issues-sidebar.component";
 import { IssuesHeaderComponent } from "./shared/issues-header/issues-header.component";
+import { LoadingService } from '../../core/services/loading.service';
+import { SpinnerComponent } from '../../shared/utils/spinner/spinner.component';
 
 @Component({
   selector: 'app-issues',
   standalone: true,
   imports: [CommonModule, FormsModule, IssuesSidebarComponent,
-    IssuesHeaderComponent, RouterOutlet],
+    IssuesHeaderComponent, RouterOutlet, SpinnerComponent],
   templateUrl: './issues.component.html',
 })
 export default class IssuesComponent {
@@ -20,4 +24,6 @@ export default class IssuesComponent {
   onIssueTypeSelected(issueType: string) {
     this.selectedIssueType = issueType;
   }
+
+  constructor(public loadingService: LoadingService) { }
 }
