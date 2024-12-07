34d11914bcb243dd9a505b158e25ae48a3f555fd
erneto13 <erneto.amaral13@gmail.com>
2024-12-07 06:51:00 -0700

    feat: agregar interfaz para crear credenciales con ID de conductor y rol opcional

--- src/app/core/interfaces/credentials.ts
+++ src/app/core/interfaces/credentials.ts
@@ -2,3 +2,10 @@ export interface Credentials {
     email: string,
     password: string,
 }
+
+export interface CreateCredentials {
+    driverId: number;
+    email: string;
+    password: string;
+    role?: string;
+}
