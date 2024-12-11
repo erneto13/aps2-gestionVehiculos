35f3700ba447af33ba8591698b3b9405adf3531a
erneto13 <erneto.amaral13@gmail.com>
2024-12-11 09:15:00 -0700

    feat: actualizar el enlace de 'Ver todas' a 'Mirar todas' y mejorar la presentación de reservas recientes

--- src/app/business/dashboard/shared/recent-bookings/recent-bookings.component.ts
+++ src/app/business/dashboard/shared/recent-bookings/recent-bookings.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { BookingResponse } from '../../../../core/interfaces/booking';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recent-bookings',
   standalone: true,
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule, DatePipe, RouterLink],
   templateUrl: './recent-bookings.component.html',
 })
 export class RecentBookingsComponent {
