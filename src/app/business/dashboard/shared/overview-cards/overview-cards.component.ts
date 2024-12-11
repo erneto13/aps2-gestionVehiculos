08b5e146ff50fc97c3e1df8f6e9140232316cc83
erneto13 <erneto.amaral13@gmail.com>
2024-12-11 09:15:09 -0700

    feat: agregar lógica para cargar y mostrar el total de contactos en las tarjetas de resumen

--- src/app/business/dashboard/shared/overview-cards/overview-cards.component.ts
+++ src/app/business/dashboard/shared/overview-cards/overview-cards.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { OverviewCard } from '../../../../core/interfaces/utils';
+import { ContactsService } from '../../../contacts/services/contacts.service';
 
 @Component({
   selector: 'app-overview-cards',
@@ -8,10 +9,26 @@ import { OverviewCard } from '../../../../core/interfaces/utils';
   imports: [CommonModule],
   templateUrl: './overview-cards.component.html',
 })
-export class OverviewCardsComponent {
+export class OverviewCardsComponent implements OnInit {
+  totalContacts!: number;
+
   @Input() cards: OverviewCard[] = [
-    { value: 2340, label: 'New products this week', percentage: 14.6, trend: 'up' },
+    { value: this.totalContacts, label: 'Contactos registrados', percentage: 14.6, trend: 'up' },
     { value: 5355, label: 'Visitors this week', percentage: 32.9, trend: 'up' },
     { value: 385, label: 'User signups this week', percentage: 2.7, trend: 'down' }
   ];
+
+  constructor(
+    private contactService: ContactsService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadCardsValues();
+  }
+
+  loadCardsValues(): void {
+    this.contactService.getContacts().forEach(() => {
+      this.totalContacts++
+    })
+  }
 }
